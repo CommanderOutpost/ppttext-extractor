d98a941aec7ce17ecce62644b10cdd2684f55096
Rasheed Amiolakitan Salami <136803958+CommanderOutpost@users.noreply.github.com>
2024-01-11 14:47:36 +0100

    improves comments

--- server/scripts/powerpoint_text_extractor.py
+++ server/scripts/powerpoint_text_extractor.py
@@ -9,9 +9,11 @@ pptx_files = os.listdir(uploads_directory)
 
 
 def extract_text_from_pptx(pptx_file):
+    # Create a Presentation object from the PowerPoint file
     prs = Presentation(pptx_file)
     all_text = []
 
+    # Iterate through each slide and extract text from shapes
     for slide in prs.slides:
         for shape in slide.shapes:
             if shape.has_text_frame:
@@ -19,18 +21,21 @@ def extract_text_from_pptx(pptx_file):
                     for run in paragraph.runs:
                         all_text.append(run.text)
 
+    # Concatenate all extracted text
     return "\n".join(all_text)
 
 
 if pptx_files:
     # Get the first .pptx file in the directory
     first_pptx_file = os.path.join(uploads_directory, pptx_files[0])
-    # print("First .pptx file in the uploads directory:", first_pptx_file)
 
-    # Now, you can extract text from the first .pptx file
+    # Extract text from the first .pptx file
     extracted_text = extract_text_from_pptx(first_pptx_file)
 
+    # Remove the processed .pptx file
     os.remove(first_pptx_file)
+
+    # Write extracted text to a new text file
     f = open("server/extracted_text.txt", "x")
     f.write(extracted_text)
     f.close()
